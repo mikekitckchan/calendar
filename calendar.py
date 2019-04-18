7202995cd1c6842f9c4d78c165179bdb9d73d26d
mikekitckchan <mikekitckchan@gmail.com>
2019-04-18 00:18:35 +0000

    initial commit

---
+++ calendar.py
@@ -0,0 +1,18 @@
+
+# A very simple Flask Hello World app for you to get started with...
+
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/calendar')
+def calendar():
+    return render_template('calendar.html')
+
+@app.route('/test')
+def test():
+    return render_template('test.html')
