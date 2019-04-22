c0ed28a8b612db66679f76364655997fec4120f7
mikekitckchan <mikekitckchan@gmail.com>
2019-04-22 07:47:53 +0000

    Adding function of POST

--- calendar.py
+++ calendar.py
@@ -1,9 +1,9 @@
-
-# A very simple Flask Hello World app for you to get started with...
-
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect, url_for
 
 app = Flask(__name__)
+
+
+event_index = 0
 
 @app.route('/')
 def index():
@@ -13,6 +13,14 @@ def index():
 def calendar():
     return render_template('calendar.html')
 
-@app.route('/test')
-def test():
-    return render_template('test.html')
+@app.route('/create', methods = ['GET', 'POST'])
+def create():
+    global event_index
+    if request.method == 'POST':
+        result = request.form.to_dict(flat=False)
+        for items in result:
+            print(items)
+        return redirect(url_for('calendar'))
+
+    else:
+        return render_template('create.html')
